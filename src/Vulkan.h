1e6dca687a7d1c5a0fef0dad859e64cefb67fde4
Massinissa Mokhtari <mokhtari.massinissa@gmail.com>
2016-07-16 15:11:03 +0200

    Add definitions in header

--- src/Vulkan.h
+++ src/Vulkan.h
@@ -4,6 +4,13 @@
 #include <vector>
 #include <GLFW\glfw3.h>
 
+#define VERTEX_BINDING_ID 0
+
+struct Vertex {
+	float position[3];
+	float color[3];
+};
+
 class Vulkan
 {
 private:
@@ -30,6 +37,9 @@ private:
 	uint32_t graphicsFamilyIndex = -1;
 	VkQueue graphicsQueue;
 
+	VkBuffer vertexBuffer;
+	VkDeviceMemory vertexMemory;
+
 public:
 	static Vulkan app;
 
@@ -46,6 +56,8 @@ private:
 	uint32_t chooseQueueFamilyIndex();
 	void createSwapchain();
 	void createSwapchainImageViews();
+	void prepareVertices();
+	uint32_t getMemoryType(uint32_t typeBits, VkFlags properties);
 	void createCommandBuffers();
 	void recordDrawCommand();
 	void createRenderPass();
